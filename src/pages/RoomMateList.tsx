232a2a4de4774b74161e37e8fd3cdec30a5f68db
Hyunsoo Kim <rlagustn805@naver.com>
2024-12-26 00:11:16 +0900

    update - map 오류 테스트

--- src/pages/RoomMateList.tsx
+++ src/pages/RoomMateList.tsx
@@ -260,6 +260,7 @@ export default function RoomMateList() {
             ) : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-gray-100 rounded-xl">
                     {data &&
+                        data.pages &&
                         data?.pages.map((page) =>
                             page.data.map((roommate) => (
                                 <div key={roommate.room_id}>
